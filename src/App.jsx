f5bfd32b64510d1f04ced34d266a0b7129c3a328
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-08 10:55:26 +0800

    Change to component routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import ErrorPage from "./pages/ErrorPage";
-import AddTodo from "./pages/AddTodo";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
 import { TodoContext } from "./contexts/TodoContext";
+import AddTodo from "./pages/AddTodo";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "add",
-    element: <AddTodo />,
-  },
-]);
+function Layout() {
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="/">Todos</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/add">Add Todo</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  );
+}
 
 export default function App() {
   const [todos, setTodos] = useLocalStorage("todos", []);
 
   return (
     <TodoContext.Provider value={{ todos, setTodos }}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="add" element={<AddTodo />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </TodoContext.Provider>
   );
 }
