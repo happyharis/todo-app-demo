83aff3b41dcdce19645280dca6db048d4ed5bd44
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-08 09:56:23 +0800

    Add card group to show todos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
-import { Container } from "react-bootstrap";
+import { useContext } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import { TodoContext } from "../contexts/TodoContext";
 
 export default function Home() {
-  return <Container>Home</Container>;
+  const todos = useContext(TodoContext).todos;
+
+  return (
+    <Container>
+      <h1 className="my-3">Your todos</h1>
+      <CardGroup todos={todos} />
+    </Container>
+  );
+}
+
+function CardGroup({ todos }) {
+  return todos.map((todo) => {
+    return (
+      <Row key={todo.key}>
+        <Col md={4}>
+          <Card className="my-3">
+            <Card.Body>
+              <Card.Title>{todo.title}</Card.Title>
+              <Card.Text>{todo.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    );
+  });
 }
