5e4580971e14dc9138852d36141712d85dc51e1d
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-08 09:30:36 +0800

    Add form add todo

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,5 +1,37 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 
 export default function AddTodo() {
-  return <Container>Add todo</Container>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <Container>
+      <h1 className="my-3">Add Todo</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            placeholder="Get software developer job"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            as="textarea"
+            rows={3}
+            placeholder={`1. Create amazing project\n2. Apply to Google & Netflix\n3. Crush interview`}
+          />
+        </Form.Group>
+        <Button variant="primary">Submit</Button>
+      </Form>
+    </Container>
+  );
 }
