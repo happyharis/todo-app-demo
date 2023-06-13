ccc93af11da10f32a5587a8ea3a6ca7e17461af8
Haris Samingan <muhamad_haris@hotmail.com>
2023-06-13 16:46:20 +0800

    Add delete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Card, Col, Container, Row, Modal, Button } from "react-bootstrap";
 import { TodoContext } from "../contexts/TodoContext";
 
 export default function Home() {
@@ -15,21 +15,66 @@ export default function Home() {
 }
 
 function CardGroup({ todos }) {
-  return todos.map((todo) => {
-    const completed = todo.completed;
-    const bg = completed ? "success" : "danger";
-    return (
-      <Col md={4} key={todo.id}>
-        <Card border={bg} className="my-3">
-          <Card.Header>{!completed && "Not"} Completed</Card.Header>
-          <Card.Body>
-            <Card.Title>{todo.title}</Card.Title>
-            <Card.Text>{todo.description}</Card.Text>
-            <Card.Link href={`todo/${todo.id}`}>Edit</Card.Link>
-            <br />
-          </Card.Body>
-        </Card>
-      </Col>
+  const setTodos = useContext(TodoContext).setTodos;
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTodoId, setSelectedTodoId] = useState(null);
+
+  const handleShowDeleteModal = (todoId) => {
+    setSelectedTodoId(todoId);
+    setShowModal(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setSelectedTodoId(null);
+    setShowModal(false);
+  };
+
+  const deleteTodo = () => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((todo) => todo.id !== selectedTodoId)
     );
-  });
+    handleCloseDeleteModal();
+  };
+
+  return (
+    <>
+      {todos.map((todo) => {
+        const completed = todo.completed;
+        const bg = completed ? "success" : "danger";
+        return (
+          <Col md={4} key={todo.id}>
+            <Card border={bg} className="my-3">
+              <Card.Header>{!completed && "Not"} Completed</Card.Header>
+              <Card.Body>
+                <Card.Title>{todo.title}</Card.Title>
+                <Card.Text>{todo.description}</Card.Text>
+                <Card.Link href={`todo/${todo.id}`}>Edit</Card.Link>
+                <Card.Link
+                  href="#"
+                  onClick={() => handleShowDeleteModal(todo.id)}
+                >
+                  Delete
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      })}
+
+      <Modal show={showModal} onHide={handleCloseDeleteModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this todo?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteModal}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={deleteTodo}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
